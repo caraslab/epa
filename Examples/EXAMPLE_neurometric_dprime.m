ccf0e9d099d5bb5e12d6c266ccde8ddbb4d55a7b
DJS <daniel.stolzberg@gmail.com>
2021-06-04 16:10:22 -0400

    example plotting

--- Examples/EXAMPLE_neurometric_dprime.m
+++ Examples/EXAMPLE_neurometric_dprime.m
@@ -18,23 +18,13 @@ par.event = "AMdepth";
 par.referenceval = 0;
 par.window = [0 1];
 
-% compute neurometric dprime for each Cluster independently and plot
-figure
-tiledlayout('flow');
-
+% compute neurometric dprime for each Cluster independently
 for i = 1:length(C)
     
     [dp,v] = C(i).neurometric_dprime(par); 
     
     C(i).neurodprime.dprime = dp;
     C(i).neurodprime.vals   = v;
-    
-    nexttile;
-    plot(C(i).neurodprime.vals,C(i).neurodprime.dprime,'-o');
-    xlabel(par.event)
-    ylabel('d''')
-    title(C(i).TitleStr);
-    grid on
 end
 
 
@@ -42,6 +32,24 @@ end
 % the original Cluster objects are already updated.
 S_AM(1).Clusters(1).neurodprime
 
+
+%% plot
+
+figure
+tiledlayout('flow');
+for i = 1:length(S_AM)
+    for j = 1:S(i).NClusters
+        
+        C = S_AM(i).Clusters(j);
+        
+        nexttile;
+        plot(C.neurodprime.vals,C.neurodprime.dprime,'-o');
+        xlabel(par.event)
+        ylabel('d''')
+        title({S(i).Name; C.TitleStr});
+        grid on
+    end
+end
 
 
 
