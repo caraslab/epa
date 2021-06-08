64c6632f6c70f5984c10295eeb8b0ba4c1f77be0
daniel <daniel.stolzberg@gmail.com>
2021-06-08 15:20:50 -0400

    updated example

--- SCRATCH_epa_examples.m
+++ SCRATCH_epa_examples.m
@@ -3,8 +3,12 @@
 
 DataPath = '/mnt/CL_4TB_2/Rose/IC recording/SUBJ-ID-228-210219-122948/Organized/210223_concat_organized';
 
+S = epa.kilosort2session(DataPath);
+
 TDTTankPath = DataPath;
-S = epa.kilosort2session(DataPath,TDTTankPath);
+S.add_TDTEvents(TDTTankPath);
+
+% S.add_TDTStreams(TDTTankPath);
 
 %% DataBrowser GUI finds valid Session objects in the base workspace
 
