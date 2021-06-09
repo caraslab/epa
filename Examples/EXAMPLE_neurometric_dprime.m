9d72a04e1999de22fe41dbe808493e065dd2e9ef
DJS <daniel.stolzberg@gmail.com>
2021-06-09 14:01:14 -0400

    fit sigmoid to data

--- Examples/EXAMPLE_neurometric_dprime.m
+++ Examples/EXAMPLE_neurometric_dprime.m
@@ -1,13 +1,22 @@
-%% Example: Compute neurometric dprime and add result as a new property to 
-% the Cluster object
+%% Example: Compute neurometric dprime and fit a sigmoidal function.
+%  Add the result as a new property to each Cluster object
+
+% first select only the relevant Session objects
+S_AM = [S.find_Session("Pre") S.find_Session("AM") S.find_Session("Post")];
 
 
-S_AM = [S.find_Session("Passive") S.find_Session("Aversive")];
 
-% process all Clusters independently
+
+% process all Clusters independently.
+% the following statement simplifies accessing all of the clusters from all
+% "S_AM" Session objects.  you can always access the original Session
+% object data for each Cluster by using C(1).Session. remember that you are
+% only copying the object handles and not the actual data, so changing data
+% in "C" will will change the original data as well.
 C = [S_AM.Clusters];
-% C = S_AM.find_Cluster("cluster582");
-% C = [C{:}];
+
+
+
 
 % add a new property, called "neurodprime" only if it doesn't already exist
 % note: you don't need to add a property in this way.  You could just make
@@ -26,39 +35,66 @@ par.modfreq = 5;
 par.metric = 'tmtf';
 
 % compute neurometric dprime for each Cluster independently
-for i = 1:length(C)
+
+
+figure(999);
+clf(999)
+tiledlayout('flow');
+
+for i = 1:numel(C)
     
-    [dp,v] = C(i).neurometric_dprime(par); 
+    % compute neurometric_dprime
+    [dp,v] = C(i).neurometric_dprime(par);
+    
+    % fit the data with a sigmoidal function
+    [xfit,yfit,p_val] = fit_sigmoid(v,dp);
+    
+    
+    
     
     C(i).neurodprime.dprime = dp;
     C(i).neurodprime.vals   = v;
+    C(i).neurodprime.xfit   = xfit;
+    C(i).neurodprime.yfit   = yfit;
+    C(i).neurodprime.p_val  = p_val;
+    
+    
+    
+    
+    nexttile
+    
+    plot(v,dp,'--o', ...
+        xfit,yfit,'-k');
+    
+    
+    xlabel(par.event)
+    ylabel('d''')
+    title({C(i).Session.Name; ...
+           C(i).TitleStr; ...
+           sprintf('\\it{p = %g}',p_val)});
+    
+    grid on
 end
 
+ax = findobj(gcf,'type','axes');
+c = cell2mat(get(ax,'ylim'));
+set(ax,'ylim',[min(c(:)) max(c(:))]);
+
+%%
 
 % Since the array C is only of handles to the orignal Cluster objects,
 % the original Cluster objects are already updated.
 S_AM(1).Clusters(1).neurodprime
 
 
-%% plot
 
-figure
-tiledlayout('flow');
-for i = 1:length(S_AM)
-    for j = 1:S(i).NClusters
-        
-        C = S_AM(i).Clusters(j);
-        
-        nexttile;
-        plot(C.neurodprime.vals,C.neurodprime.dprime,'-o');
-        xlabel(par.event)
-        ylabel('d''')
-        title({C.Session.Name; C.TitleStr});
-        grid on
-    end
-end
 
-%% fit
 
-% x = 
+
+
+
+
+
+
+
 
